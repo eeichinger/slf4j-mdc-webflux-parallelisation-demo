b5696f77d39504f1d82b37d706fb6602bde5df80
Erich Eichinger <eeichinger@gmail.com>
2022-04-25 16:20:26 +0200

    initial checkin

---
+++ src/main/java/com/example/slf4jmdcdemo/Slf4jMdcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.slf4jmdcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jMdcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slf4jMdcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slf4jmdcdemo/Slf4jMdcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slf4jmdcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4jMdcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
